0d270c4b498381011d51017201c9c9a4e9e5e8d3
Maulik Jyani <53288182+jyanimaulik@users.noreply.github.com>
2023-09-02 21:20:03 -0700

    Update RisingTemperature.sql

--- Questions/RisingTemperature.sql
+++ Questions/RisingTemperature.sql
@@ -1,1 +1,13 @@
+#In SQL, id is the primary key for this table.
+#This table contains information about the temperature on a certain day.
+
+#Find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+#Return the result table in any order.
+#The result format is in the following example.
+
+# Write your MySQL query statement below
+
+SELECT w1.id
+FROM Weather w1, Weather w2
+WHERE DATEDIFF(w1.recordDate,w2.recordDate) =  1 AND w1.temperature > w2.temperature
 
