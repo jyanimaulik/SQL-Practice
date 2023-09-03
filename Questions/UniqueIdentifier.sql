c0d597f7b52e09a4a1156b60b04399faffb7352e
Maulik Jyani <53288182+jyanimaulik@users.noreply.github.com>
2023-09-02 20:28:03 -0700

    Create UniqueIdentifier.sql

---
+++ Questions/UniqueIdentifier.sql
@@ -0,0 +1,5 @@
+Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
+
+Return the result table in any order.
+
+The result format is in the following example.
