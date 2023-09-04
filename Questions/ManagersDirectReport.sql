772ed5fa9be908426ecc4440fe0aa86ba8bdd885
Maulik Jyani <53288182+jyanimaulik@users.noreply.github.com>
2023-09-04 00:31:30 -0700

    Update ManagersDirectReport.sql

--- Questions/ManagersDirectReport.sql
+++ Questions/ManagersDirectReport.sql
@@ -8,9 +8,12 @@
 # The result format is in the following example.
 
 # Write your MySQL query statement below
-SELECT e2.name
+# Write your MySQL query statement below
+
+
+SELECT e1.name
 FROM Employee e1
 INNER JOIN Employee e2
-ON e1.managerId = e2.id
-GROUP BY e1.managerId
-HAVING COUNT(e1.id) >= 5;
+ON e1.id = e2.managerId
+GROUP BY e2.managerId
+HAVING COUNT(e2.managerId) >= 5;
