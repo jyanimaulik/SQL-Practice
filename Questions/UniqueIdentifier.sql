ed9c5d2b8dcdf820784beac0707b7c9930b5f099
Maulik Jyani <53288182+jyanimaulik@users.noreply.github.com>
2023-09-04 23:24:37 -0700

    Update UniqueIdentifier.sql

--- Questions/UniqueIdentifier.sql
+++ Questions/UniqueIdentifier.sql
@@ -1,5 +1,11 @@
-Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
+#Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
 
-Return the result table in any order.
+#Return the result table in any order.
 
-The result format is in the following example.
+#The result format is in the following example.
+
+# Write your MySQL query statement below
+select unique_id, name from Employees as e
+left join EmployeeUNI as c
+on e.id = c.id
+  
